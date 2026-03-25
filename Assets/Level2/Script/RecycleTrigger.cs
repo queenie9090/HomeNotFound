b704d75986a16391ad5e5c27a45d55c1ebd03f47
winyee <154496935+koh123456789@users.noreply.github.com>
2026-03-26 01:31:43 +0800

    Add constraint
    not letting player sell recycle items before they discover they can do so

--- Assets/Level2/Script/RecycleTrigger.cs
+++ Assets/Level2/Script/RecycleTrigger.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class RecycleTrigger : MonoBehaviour
-{   
+{
+
+    public EnableRecycle enableRecycle;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -10,6 +14,7 @@ public class RecycleTrigger : MonoBehaviour
 
             if (gm.currentState == Level2Manager.GameState.NeedMoney)
             {
+                enableRecycle.SetActiveRecycle();
                 gm.SetState(Level2Manager.GameState.DiscoverRecycle);
             }
         }
